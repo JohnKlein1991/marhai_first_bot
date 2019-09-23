585020903b169f9528df1c01a2dda3c574e51c7f
andrew_marhai <buk2018irinam@gmail.com>
2019-09-23 13:26:11 +0300

    Edit uri for setWebhook. Part 2))

--- app/Http/Controllers/Backend/SettingController.php
+++ app/Http/Controllers/Backend/SettingController.php
@@ -34,7 +34,7 @@ class SettingController extends Controller
             'setwebhook',
             [
             'query' => [
-                'url' => $request->url()
+                'url' => $request->url
             ]
         ]);
         return redirect()->route('admin.setting.index')->with('status',  $result);
